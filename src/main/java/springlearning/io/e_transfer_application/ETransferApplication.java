513e5825f6e7d8ef493a841cf9149c60f4782fa6
mohnish <mohnishratanpal@gmail.com>
2026-01-04 00:16:37 -0500

    chore: bootstrap Spring Boot project via start.spring.io

---
+++ src/main/java/springlearning/io/e_transfer_application/ETransferApplication.java
@@ -0,0 +1,13 @@
+package springlearning.io.e_transfer_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ETransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ETransferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springlearning/io/e_transfer_application/ETransferApplicationTests.java
@@ -0,0 +1,13 @@
+package springlearning.io.e_transfer_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ETransferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
